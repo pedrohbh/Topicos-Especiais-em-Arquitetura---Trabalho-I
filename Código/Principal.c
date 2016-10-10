7ae5784991b6d05d0d5621dfd7303a72dde8b0f6
Lab <adm@adm.com>
2016-10-10 15:01:59 -0300

    Segundo Commit

--- Código/Principal.c
+++ Código/Principal.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
 
 
-void multiplicacaoMatrixFloat( float *a, float *b )
+void multiplicacaoMatrixFloat( float *m, float *n, float *p, int tamanho )
 {
-
+	int i;
+	int j;
+	int k;
+	for ( i = 0; i < tamanho; ++i )
+		for ( j = 0; j < tamanho; ++j )
+		{
+			double soma = 0;
+			for ( k = 0; k < tamanho; ++k )
+			{
+				double a = m[ i * tamanho + k ];
+				double b = n[ k * tamanho + j ];
+				soma += a * b;
+			}
+			p[ i * tamanho + j ] = soma;
+		}
 }
 
 
